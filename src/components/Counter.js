1f79929efbf8c910223cc740f702db3886e3dfc8
Mark Reed <mreed@Admins-MacBook-Pro.local>
2022-10-25 19:01:41 -0400

    custom hook for page load

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import countapi from 'countapi-js'
 
 function Counter(props) {
-    const [ count, setCount ] = useState(0)
+    const [ count, setCount ] = useState("")
+    // useGetInitialCount gets current value on page load without incrementing count
+    const useGetInitialCount = () => {
+        useEffect(() => {
+            countapi.get('fearless.tech', '1ccb732e-b55a-4404-ad3f-0f99c02fe44e')
+            .then((result) => {
+                setCount(result.value)
+            })
+        }, [])
+    }
+    useGetInitialCount()
+    
+    // incrementCount gets called on every button click
     const incrementCount = () => {
         countapi.hit('fearless.tech', '1ccb732e-b55a-4404-ad3f-0f99c02fe44e')
             .then((result) => {
-                console.log(result)
                 setCount(result.value)
             })
     }
